de0ae34f1a4b2749322eefd7152f4e084d3762b0
Stranik <rund2504@gmail.com>
2021-02-11 20:45:10 +0300

    Add interface IModel

---
+++ PerimetrApp/IModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerimetrApp
+{
+    /// <summary>
+    /// Протокол, для разработки подсчета периметра прямоугольника
+    /// </summary>
+    public interface IModel
+    {
+        /// <summary>
+        /// Метод, возращающий периметр прямоугльника по двум сторонам
+        /// </summary>
+        /// <param name="a">Одна сторона прямоугольника</param>
+        /// <param name="b">Другая сторона прямоугольника</param>
+        /// <returns>Периметр прямоугольника</returns>
+        int GetP(int a, int b);
+    }
+}
